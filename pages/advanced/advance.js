180ac456d402911e3e38e2264d590f99d54b7032
pou26 <poushaliaich1999@gmail.com>
2023-05-02 12:29:57 +0530

    final

--- pages/advanced/advance.js
+++ pages/advanced/advance.js
@@ -19,12 +19,12 @@ const advance = () => {
     {/* //..................Treatments..................// */}
 
     <h1 className={styles.headingwrap}>
-        Advanced Infertility Treatments
+        {"Advanced Infertility Treatments"}
     </h1>
     <hr className={styles.linewrap}/>
     <h1 className={styles.headingmwrap}>
     
-    Freezing or Preservation
+    {"Freezing or Preservation"}
     </h1>
     <div className={styles.bodywrap}>
     <Image
@@ -37,13 +37,13 @@ const advance = () => {
 
       {/* <img className={styles.imgwrap} src='/Advance Features/Freezing or Preservation.png' width="400" height="300" alt=''/> */}
 
-<p className={styles.pwrap}>Freezing or preservation of reproductive material is a common technique used in infertility treatments. It allows individuals to preserve their eggs, sperm, or embryos for future use, especially for those who are undergoing medical treatments that may affect their fertility or for those who want to delay parenthood. The process involves the collection and freezing of reproductive material, which can be stored for years without losing its quality. When the individual or couple is ready to conceive, the frozen material can be thawed and used in assisted reproduction techniques such as IVF. Freezing or preservation of reproductive material offers a valuable option for individuals or couples who want to ensure their future chances of conception.</p>
+<p className={styles.pwrap}>{"Freezing or preservation of reproductive material is a common technique used in infertility treatments. It allows individuals to preserve their eggs, sperm, or embryos for future use, especially for those who are undergoing medical treatments that may affect their fertility or for those who want to delay parenthood. The process involves the collection and freezing of reproductive material, which can be stored for years without losing its quality. When the individual or couple is ready to conceive, the frozen material can be thawed and used in assisted reproduction techniques such as IVF. Freezing or preservation of reproductive material offers a valuable option for individuals or couples who want to ensure their future chances of conception."}</p>
 </div>
     <h1 className={styles.headingmwrap}>
-    MACS
+    {"MACS"}
     </h1>
 <div className={styles.bodywrap}>
-<p className={styles.pwrap}>Magnetic-activated cell sorting (MACS) is a specialized laboratory technique used in infertility treatments to improve the quality of sperm. MACS is a type of sperm selection method that allows for the isolation and separation of high-quality sperm from those with abnormalities. The process involves attaching magnetic beads to the sperm cells and passing them through a magnetic field. The high-quality sperm cells with normal DNA undergo magnetic attraction and are collected, while the abnormal sperm cells remain behind. This technique helps increase the chances of fertilization and embryo development, especially in cases where male partners have sperm DNA fragmentation or poor sperm quality. MACS can be used in combination with other assisted reproductive techniques, such as ICSI, to improve treatment outcomes</p>
+<p className={styles.pwrap}>{"Magnetic-activated cell sorting (MACS) is a specialized laboratory technique used in infertility treatments to improve the quality of sperm. MACS is a type of sperm selection method that allows for the isolation and separation of high-quality sperm from those with abnormalities. The process involves attaching magnetic beads to the sperm cells and passing them through a magnetic field. The high-quality sperm cells with normal DNA undergo magnetic attraction and are collected, while the abnormal sperm cells remain behind. This technique helps increase the chances of fertilization and embryo development, especially in cases where male partners have sperm DNA fragmentation or poor sperm quality. MACS can be used in combination with other assisted reproductive techniques, such as ICSI, to improve treatment outcomes"}</p>
 <Image
   src="/Advance Features/MACS.png"
   className={styles.imgwrap}
@@ -56,7 +56,7 @@ const advance = () => {
 </div>
     <h1 className={styles.headingmwrap}>
     
-    PICSI
+    {"PICSI"}
     </h1>
 <div className={styles.bodywrap}>
 
@@ -68,15 +68,15 @@ const advance = () => {
   height={300}
 />
 {/* <img className={styles.imgwrap} src='/Advance Features/PICSI.png' width="400" height="300" alt=''/> */}
-<p className={styles.pwrap}>PICSI (Physiological Intracytoplasmic Sperm Injection) is an advanced laboratory technique used in infertility treatments to improve the selection of the healthiest and most viable sperm for fertilization. This technique involves the use of a special dish that contains a hyaluronan gel that mimics the environment of the female reproductive tract. The sperm sample is placed in the dish, and the healthiest sperm attach to the hyaluronan, indicating that they have the necessary structural and functional characteristics for successful fertilization.PICSI is particularly useful for couples who have had unsuccessful IVF treatments or have male factor infertility issues. This technique can increase the chances of successful fertilization and improve the overall success rates of IVF treatments.</p>
+<p className={styles.pwrap}>{"PICSI (Physiological Intracytoplasmic Sperm Injection) is an advanced laboratory technique used in infertility treatments to improve the selection of the healthiest and most viable sperm for fertilization. This technique involves the use of a special dish that contains a hyaluronan gel that mimics the environment of the female reproductive tract. The sperm sample is placed in the dish, and the healthiest sperm attach to the hyaluronan, indicating that they have the necessary structural and functional characteristics for successful fertilization.PICSI is particularly useful for couples who have had unsuccessful IVF treatments or have male factor infertility issues. This technique can increase the chances of successful fertilization and improve the overall success rates of IVF treatments."}</p>
 
 </div>
     <h1 className={styles.headingmwrap}>
-    Sequential transfer
+    {"Sequential transfer"}
     </h1>
 <div className={styles.bodywrap}>
 <p className={styles.pwrap}>
-Sequential transfer is a technique used in infertility treatments, particularly in in-vitro fertilization (IVF), where the embryos are transferred into the uterus in a specific order. It involves transferring a selected number of embryos into the uterus, followed by a second transfer of any remaining embryos after a few days. This technique aims to improve the chances of pregnancy by allowing embryologists to carefully choose the best embryos for the first transfer, and then evaluate the remaining embryos for further transfer in a subsequent cycle. Sequential transfer is often used when a woman has had multiple unsuccessful IVF cycles, or when there is a risk of multiple pregnancies if all embryos are transferred at once.</p>
+{"Sequential transfer is a technique used in infertility treatments, particularly in in-vitro fertilization (IVF), where the embryos are transferred into the uterus in a specific order. It involves transferring a selected number of embryos into the uterus, followed by a second transfer of any remaining embryos after a few days. This technique aims to improve the chances of pregnancy by allowing embryologists to carefully choose the best embryos for the first transfer, and then evaluate the remaining embryos for further transfer in a subsequent cycle. Sequential transfer is often used when a woman has had multiple unsuccessful IVF cycles, or when there is a risk of multiple pregnancies if all embryos are transferred at once."}</p>
 <Image
   src="/Advance Features/Sequential Transfer.png"
   className={styles.imgwrap}
@@ -89,7 +89,7 @@ Sequential transfer is a technique used in infertility treatments, particularly 
 </div>
     <h1 className={styles.headingmwrap}>
     
-    Endometrial Rejuvenation
+    {"Endometrial Rejuvenation"}
     </h1>
 <div className={styles.bodywrap}>
 <Image
@@ -102,7 +102,7 @@ Sequential transfer is a technique used in infertility treatments, particularly 
 
 {/* <img className={styles.imgwrap} src='/Advance Features/Endometrial Rejuvenation.png' width="400" height="300" alt=''/> */}
 <p className={styles.pwrap}>
-Endometrial rejuvenation is a technique used in infertility treatments to improve the chances of successful embryo implantation. It involves the use of growth factors or stem cells to regenerate the lining of the uterus, which plays a crucial role in supporting embryo development. This technique is particularly useful for women who have experienced recurrent implantation failure, thin endometrial lining, or who have undergone multiple unsuccessful IVF cycles. Endometrial rejuvenation can be done through various methods such as stem cell therapy, PRP (platelet-rich plasma) therapy, or through the use of growth factors. Endometrial rejuvenation has shown promising results in improving endometrial receptivity, and thus increasing the chances of a successful pregnancy.</p>
+{"Endometrial rejuvenation is a technique used in infertility treatments to improve the chances of successful embryo implantation. It involves the use of growth factors or stem cells to regenerate the lining of the uterus, which plays a crucial role in supporting embryo development. This technique is particularly useful for women who have experienced recurrent implantation failure, thin endometrial lining, or who have undergone multiple unsuccessful IVF cycles. Endometrial rejuvenation can be done through various methods such as stem cell therapy, PRP (platelet-rich plasma) therapy, or through the use of growth factors. Endometrial rejuvenation has shown promising results in improving endometrial receptivity, and thus increasing the chances of a successful pregnancy."}</p>
 
 </div>
 
--- pages/female/femaleinfertility.js
+++ pages/female/femaleinfertility.js
@@ -44,7 +44,7 @@ const femaleinfertility = () => {
        {"Hormonal imbalances"}
     </h2>
         <p className={styles.pwrap}>
-        Hormonal imbalances are a common cause of female infertility. These imbalances can prevent ovulation or interfere with the implantation of a fertilized egg. Polycystic ovary syndrome (PCOS) is a common hormonal disorder that can cause irregular periods, anovulation, and infertility. Other hormonal imbalances that can cause infertility include thyroid disorders, hyperprolactinemia, and premature ovarian failure. Treatment options for hormonal imbalances may include medication to regulate ovulation and hormone levels
+        {"Hormonal imbalances are a common cause of female infertility. These imbalances can prevent ovulation or interfere with the implantation of a fertilized egg. Polycystic ovary syndrome (PCOS) is a common hormonal disorder that can cause irregular periods, anovulation, and infertility. Other hormonal imbalances that can cause infertility include thyroid disorders, hyperprolactinemia, and premature ovarian failure. Treatment options for hormonal imbalances may include medication to regulate ovulation and hormone levels"}
         </p>
     </div>
     <div className={styles.imgmwrap}>
@@ -61,10 +61,10 @@ const femaleinfertility = () => {
     <div className={styles.mwrapper}>
     <div className={styles.mwrap}>
     <h2 className={styles.hwrap}>
-    Structural issues
+    {"Structural issues"}
     </h2>
         <p className={styles.pwrap}>
-        Structural issues such as blocked fallopian tubes, uterine abnormalities, and endometriosis can also cause female infertility. Blocked fallopian tubes can prevent the fertilization of an egg or the implantation of a fertilized egg, while uterine abnormalities such as fibroids or polyps can interfere with implantation. Endometriosis is a condition where tissue similar to the lining of the uterus grows outside of the uterus, causing inflammation and scarring that can affect fertility. Treatment options for structural issues may include surgery to correct abnormalities or assisted reproductive technologies such as IVF.
+          {"Structural issues such as blocked fallopian tubes, uterine abnormalities, and endometriosis can also cause female infertility. Blocked fallopian tubes can prevent the fertilization of an egg or the implantation of a fertilized egg, while uterine abnormalities such as fibroids or polyps can interfere with implantation. Endometriosis is a condition where tissue similar to the lining of the uterus grows outside of the uterus, causing inflammation and scarring that can affect fertility. Treatment options for structural issues may include surgery to correct abnormalities or assisted reproductive technologies such as IVF."}
         </p>
     </div>
     <div className={styles.imgmwrap}>
@@ -82,12 +82,12 @@ const femaleinfertility = () => {
     {/* //..................Treatments..................// */}
 
     <h1 className={styles.headingwrap}>
-        Advanced Female Infertility Treatments
+        {"Advanced Female Infertility Treatments"}
     </h1>
     <hr className={styles.linewrap}/>
     <h1 className={styles.headingmwrap}>
     
-    OI/IT
+    {"OI/IT"}
     </h1>
     <div className={styles.bodywrap}>
     <Image
@@ -105,9 +105,7 @@ const femaleinfertility = () => {
     ICSI
     </h1>
 <div className={styles.bodywrap}>
-<p className={styles.pwrap}> Intracytoplasmic Sperm Injection, which is a type of assisted reproductive technology (ART) used to treat male infertility. In this procedure, a single sperm is injected directly into an egg to achieve fertilization. ICSI is often recommended when the male partner has low sperm count, poor sperm motility, or abnormal sperm shape.
-
-ICSI is a highly successful and safe procedure, with success rates comparable to conventional IVF. It has revolutionized the treatment of male infertility, providing hope for couples who would not have been able to conceive otherwise. However, as with any fertility treatment, it is important to understand the risks and benefits before proceeding. A qualified healthcare professional can help you determine if ICSI is the right option for you</p>
+<p className={styles.pwrap}>{ "Intracytoplasmic Sperm Injection, which is a type of assisted reproductive technology (ART) used to treat male infertility. In this procedure, a single sperm is injected directly into an egg to achieve fertilization. ICSI is often recommended when the male partner has low sperm count, poor sperm motility, or abnormal sperm shape.ICSI is a highly successful and safe procedure, with success rates comparable to conventional IVF. It has revolutionized the treatment of male infertility, providing hope for couples who would not have been able to conceive otherwise. However, as with any fertility treatment, it is important to understand the risks and benefits before proceeding. A qualified healthcare professional can help you determine if ICSI is the right option for you"}</p>
 <Image
   src="/Female Fertility Icon/ICSI.png"
   className={styles.imgwrap}
@@ -120,7 +118,7 @@ ICSI is a highly successful and safe procedure, with success rates comparable to
 </div>
     <h1 className={styles.headingmwrap}>
     
-    IUI
+    {"IUI"}
     </h1>
 <div className={styles.bodywrap}>
 <Image
@@ -132,21 +130,15 @@ ICSI is a highly successful and safe procedure, with success rates comparable to
 />
 
 {/* <img className={styles.imgwrap} src='/Female Fertility Icon/IUI.png' width="400" height="300" alt=''/> */}
-<p className={styles.pwrap}>IUI, or intrauterine insemination, is a type of fertility treatment that involves the placement of sperm directly into the uterus through a catheter. It is often used to treat couples who are struggling to conceive due to issues with sperm count, motility, or cervical mucus.
-
-The procedure is relatively simple and painless, and can be done in a clinic or doctor's office. It is typically timed with the woman's ovulation cycle to increase the chances of fertilization.
-
-IUI is a less invasive and less expensive option compared to other fertility treatments such as IVF. However, success rates can vary depending on individual factors such as age, cause of infertility, and overall health. It is important to consult with a healthcare professional to determine if IUI is the best option for your unique situation.</p>
+<p className={styles.pwrap}>{"IUI, or intrauterine insemination, is a type of fertility treatment that involves the placement of sperm directly into the uterus through a catheter. It is often used to treat couples who are struggling to conceive due to issues with sperm count, motility, or cervical mucus.The procedure is relatively simple and painless, and can be done in a clinic or doctor's office. It is typically timed with the woman's ovulation cycle to increase the chances of fertilization.IUI is a less invasive and less expensive option compared to other fertility treatments such as IVF. However, success rates can vary depending on individual factors such as age, cause of infertility, and overall health. It is important to consult with a healthcare professional to determine if IUI is the best option for your unique situation."}</p>
 
 </div>
     <h1 className={styles.headingmwrap}>
-    IVF
+    {"IVF"}
     </h1>
 <div className={styles.bodywrap}>
 <p className={styles.pwrap}>
-IVF, or in vitro fertilization, is a fertility treatment that involves the retrieval of a woman's eggs, fertilization with sperm in a laboratory setting, and transfer of the resulting embryos into the uterus. It is a highly effective option for couples struggling with infertility due to various factors such as advanced maternal age, blocked fallopian tubes, or low sperm count.
-
-IVF is a complex and expensive procedure, but can offer hope to couples who have been unable to conceive through other methods. Success rates can vary based on age, health status, and other factors. It is important to consult with a qualified healthcare professional to determine if IVF is the best option for your individual situation</p>
+{"IVF, or in vitro fertilization, is a fertility treatment that involves the retrieval of a woman's eggs, fertilization with sperm in a laboratory setting, and transfer of the resulting embryos into the uterus. It is a highly effective option for couples struggling with infertility due to various factors such as advanced maternal age, blocked fallopian tubes, or low sperm count.IVF is a complex and expensive procedure, but can offer hope to couples who have been unable to conceive through other methods. Success rates can vary based on age, health status, and other factors. It is important to consult with a qualified healthcare professional to determine if IVF is the best option for your individual situation"}</p>
 <Image
   src="/Female Fertility Icon/IVF.png"
   className={styles.imgwrap}
@@ -159,7 +151,7 @@ IVF is a complex and expensive procedure, but can offer hope to couples who have
 </div>
     <h1 className={styles.headingmwrap}>
     
-    Embryo Donation
+    {"Embryo Donation"}
     </h1>
 <div className={styles.bodywrap}>
 <Image
@@ -171,13 +163,13 @@ IVF is a complex and expensive procedure, but can offer hope to couples who have
 />
 
 {/* <img className={styles.imgwrap} src='/Female Fertility Icon/Embryo Donation.png' width="400" height="300" alt=''/> */}
-<p className={styles.pwrap}>Embryo donation is a form of fertility treatment in which frozen embryos from one couple are donated to another couple for use in their IVF treatment. These embryos are usually created during the IVF process when a couple has excess viable embryos that they do not need for their own treatment.Embryo donation can provide an opportunity for couples who are unable to conceive on their own to start or grow their family. The donated embryos are transferred to the uterus of the recipient woman, who carries the pregnancy to term and gives birth to the child.Embryo donation is a highly regulated process, and both the donor and recipient couples typically undergo thorough medical and psychological screening before the procedure can take place</p>
+<p className={styles.pwrap}>{"Embryo donation is a form of fertility treatment in which frozen embryos from one couple are donated to another couple for use in their IVF treatment. These embryos are usually created during the IVF process when a couple has excess viable embryos that they do not need for their own treatment.Embryo donation can provide an opportunity for couples who are unable to conceive on their own to start or grow their family. The donated embryos are transferred to the uterus of the recipient woman, who carries the pregnancy to term and gives birth to the child.Embryo donation is a highly regulated process, and both the donor and recipient couples typically undergo thorough medical and psychological screening before the procedure can take place"}</p>
 
 </div>
     <hr className={styles.linewrap}/>
     <h1 className={styles.headingmwrap}>
     
-    Laparoscopy
+    {"Laparoscopy"}
     </h1>
     <div className={styles.bodywrap}>
     <Image
@@ -189,10 +181,10 @@ IVF is a complex and expensive procedure, but can offer hope to couples who have
 />
       {/* <img className={styles.imgwrap} src='/Female Fertility Icon/Laparoscopy .png' width="400" height="300" alt=''/> */}
 
-<p className={styles.pwrap}>Laparoscopy is a minimally invasive surgical procedure that allows doctors to examine and treat medical conditions in the abdomen and pelvis. During the procedure, a thin, lighted tube with a camera attached to it is inserted through a small incision in the abdomen, allowing the doctor to view the internal organs on a monitor.Laparoscopy can be used to diagnose and treat a variety of conditions, including endometriosis, infertility, pelvic pain, and ovarian cysts. The procedure typically has a shorter recovery time and fewer complications than traditional open surgery, making it a preferred option for many patients.Laparoscopy is often performed under general anesthesia and may require only a few small incisions, resulting in less pain and scarring than traditional surgery.</p>
+<p className={styles.pwrap}>{"Laparoscopy is a minimally invasive surgical procedure that allows doctors to examine and treat medical conditions in the abdomen and pelvis. During the procedure, a thin, lighted tube with a camera attached to it is inserted through a small incision in the abdomen, allowing the doctor to view the internal organs on a monitor.Laparoscopy can be used to diagnose and treat a variety of conditions, including endometriosis, infertility, pelvic pain, and ovarian cysts. The procedure typically has a shorter recovery time and fewer complications than traditional open surgery, making it a preferred option for many patients.Laparoscopy is often performed under general anesthesia and may require only a few small incisions, resulting in less pain and scarring than traditional surgery."}</p>
 </div>
     <h1 className={styles.headingmwrap}>
-    Hysteroscopy
+    {"Hysteroscopy"}
     </h1>
 <div className={styles.bodywrap}>
 <p className={styles.pwrap}>Hysteroscopy is a minimally invasive diagnostic and surgical procedure that allows doctors to examine the inside of the uterus using a thin, lighted tube with a camera attached to it. The procedure is typically performed under general anesthesia and involves inserting the hysteroscope through the cervix and into the uterus.Hysteroscopy can be used to diagnose and treat a variety of conditions, including abnormal bleeding, uterine fibroids, polyps, and adhesions. The procedure allows doctors to visualize the inside of the uterus and take biopsies or remove abnormal tissue without the need for traditional open surgery.Hysteroscopy is generally considered safe and has a low risk of complications. The recovery time is typically short, with most women able to return to their normal activities within a day or two.</p>
@@ -208,7 +200,7 @@ IVF is a complex and expensive procedure, but can offer hope to couples who have
 </div>
     <h1 className={styles.headingmwrap}>
     
-    Surrogacy
+    {"Surrogacy"}
     </h1>
 <div className={styles.bodywrap}>
 <Image
@@ -220,15 +212,15 @@ IVF is a complex and expensive procedure, but can offer hope to couples who have
 />
 
 {/* <img className={styles.imgwrap} src='/Female Fertility Icon/Surrogacy.png' width="400" height="300" alt=''/> */}
-<p className={styles.pwrap}>Surrogacy is a process in which a woman, known as the surrogate mother, carries a pregnancy and gives birth to a child for another person or couple, known as the intended parents. There are two types of surrogacy: traditional and gestational. In traditional surrogacy, the surrogate mother is inseminated with the intended father's sperm, while in gestational surrogacy, the embryo is created using the intended parents' or donors' sperm and eggs and transferred to the surrogate mother's uterus.Surrogacy can be an option for couples who are unable to conceive naturally or carry a pregnancy to term due to medical reasons.</p>
+<p className={styles.pwrap}>{"Surrogacy is a process in which a woman, known as the surrogate mother, carries a pregnancy and gives birth to a child for another person or couple, known as the intended parents. There are two types of surrogacy: traditional and gestational. In traditional surrogacy, the surrogate mother is inseminated with the intended father's sperm, while in gestational surrogacy, the embryo is created using the intended parents' or donors' sperm and eggs and transferred to the surrogate mother's uterus.Surrogacy can be an option for couples who are unable to conceive naturally or carry a pregnancy to term due to medical reasons."}</p>
 
 </div>
     <h1 className={styles.headingmwrap}>
-    Egg donation
+    {"Egg donation"}
     </h1>
 <div className={styles.bodywrap}>
 <p className={styles.pwrap}>
-Egg donation is a process in which a woman, known as the donor, provides her eggs to another person or couple, known as the recipient(s), for use in assisted reproduction. This can be an option for women who are unable to produce viable eggs due to medical reasons or advanced age. Egg donation involves the use of fertility drugs to stimulate the ovaries to produce multiple eggs, which are then retrieved through a minor surgical procedure. The eggs are fertilized with sperm in a laboratory and transferred to the recipient's uterus or used for embryo creation. Egg donation can offer a chance at parenthood for those who would otherwise be unable to conceive.</p>
+{"Egg donation is a process in which a woman, known as the donor, provides her eggs to another person or couple, known as the recipient(s), for use in assisted reproduction. This can be an option for women who are unable to produce viable eggs due to medical reasons or advanced age. Egg donation involves the use of fertility drugs to stimulate the ovaries to produce multiple eggs, which are then retrieved through a minor surgical procedure. The eggs are fertilized with sperm in a laboratory and transferred to the recipient's uterus or used for embryo creation. Egg donation can offer a chance at parenthood for those who would otherwise be unable to conceive."}</p>
 <Image
   src="/Female Fertility Icon/Egg Donation.png"
   className={styles.imgwrap}
