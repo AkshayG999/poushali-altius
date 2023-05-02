346924a72739ac9e91c6dc8ec5734bf9ce02d77c
pou26 <poushaliaich1999@gmail.com>
2023-05-02 10:32:53 +0530

    final

--- components/Youtube.js
+++ components/Youtube.js
@@ -8,7 +8,7 @@ const Youtube = () => {
         <h1 className={styles.containerheading}>{"Testimonials"}</h1>
         <hr className={styles.line}/>
         <div className={styles.containeryoutube}>
-            <iframe src="https://www.youtube.com/embed/ZrfshZz57tA" className="shadow-1-strong rounded" title="Altius-hospital" allowFullScreen></iframe>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/ZrfshZz57tA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
         </div>
       </Container>
     </div>
